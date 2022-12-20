9103690bda78c8eb763ecc40e2c0c77f7f008f7f
Dave <davidlongswe@gmail.com>
2022-12-21 00:04:40 +0100

    structure

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,18 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;500;600;700;800&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body className="font-raleway">
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }

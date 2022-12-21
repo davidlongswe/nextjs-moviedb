bb5077d8adb4f6110a45f0d9b627c691df8ac40b
Dave <davidlongswe@gmail.com>
2022-12-22 00:11:40 +0100

    movie api call working

--- components/index.js
+++ components/index.js
@@ -1,2 +1,5 @@
 export { default as Header } from "../components/Header/Header";
 export { default as Hero } from "../components/Hero/Hero";
+export { default as Card } from "../components/Card/Card";
+export { default as Spinner } from "../components/Spinner/Spinner";
+export { default as Grid } from "../components/Grid/Grid";
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import type { NextPage } from "next";
+
+//Hooks
+import { useFetchMovies } from "../api/fetchHooks";
 
 //Components
-import { Header } from "../components";
+import { Header, Spinner, Grid, Card, Hero } from "../components";
 
-const HomePage = () => {
+const HomePage: NextPage = () => {
+  const [query, setQuery] = React.useState("");
+
+  const { data, fetchNextPage, isLoading, isFetching, error } =
+    useFetchMovies(query);
+
+  console.log(data);
+
   return (
     <main className="relative h-screen overflow-y-scroll">
       <Header />
+      <Hero />
+      <Grid />
+      <Card />
+      <Spinner />
     </main>
   );
 };
